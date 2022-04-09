e6ac6397388a79fe2c83aff9ba8a3f6071361738
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-04-09 17:30:27 -0400

    accidentally trashed a bunch of changes... smh

--- src/components/BuildingHistory.vue
+++ src/components/BuildingHistory.vue
@@ -1,28 +1,21 @@
 <template>
-	<div class="mdc-card">
-		<span>{{ store.name }}</span>
-		<br />
-		<span>{{ store.entries }}</span>
-	</div>
+  <div class="mdc-card">
+    <h3 class="mdc-card__title">{{ store.name }}</h3>
+    <br />
+    <ul v-for="e in store.entries">
+      <li>{{ e }}</li>
+    </ul>
+  </div>
 </template>
-<script >
-import { useBuildingOne } from '@/stores/buildingOne.js'
+<script setup>
+import { useBuildingOne } from "@/stores/buildingOne.js";
 
-export default {
-	setup() {
-
-		const store = useBuildingOne()
-		store.fetchName()
-		store.fetchEntries()
-
-		return {
-			store,
-		}
-
-	}
-}
+const store = useBuildingOne();
+store.fetchName();
+store.fetchEntries();
 
 // export default {
 // 	beforeMount
-// }</script>
+// }
+</script>
 <style></style>
