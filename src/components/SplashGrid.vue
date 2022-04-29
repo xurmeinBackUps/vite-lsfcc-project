00ffe8ac186e14175c5a16176e9a3516c4216e53
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-04-29 07:12:57 -0400

    migration to self-hosting assets still in-progress

--- src/components/SplashGrid.vue
+++ src/components/SplashGrid.vue
@@ -3,7 +3,7 @@
     <router-link :to="{ name: 'building-show', params: { bId: 'building1' } }">
       <splash-card
         card-title="Building One"
-        image="https://firebasestorage.googleapis.com/v0/b/lsfcc-project-moco.appspot.com/o/pictures%2Fbuildings%2Flincoln-building-1b.jpg?alt=media&token=77c3906c-4e6c-4dee-8506-66d3eacfd7af"
+        image="@/assets/images/lincoln-building-1a.jpg"
       >
       </splash-card>
     </router-link>
@@ -11,7 +11,7 @@
     <router-link :to="{ name: 'building-show', params: { bId: 'building2' } }">
       <splash-card
         card-title="Building Two"
-        image="https://firebasestorage.googleapis.com/v0/b/lsfcc-project-moco.appspot.com/o/pictures%2Fbuildings%2Flincoln-building-2.jpg?alt=media&token=4554318f-f953-4465-a177-a5f8d24971d2"
+        image="@/assets/images/lincoln-building-2.jpg"
       >
       </splash-card>
     </router-link>
@@ -19,7 +19,7 @@
     <router-link to="/profiles">
       <splash-card
         card-title="Faculty & Staff"
-        image="https://firebasestorage.googleapis.com/v0/b/lsfcc-project-moco.appspot.com/o/icons%2Fyearbook.png?alt=media&token=a5917959-41c7-4d50-8b1a-97985f76cb4e"
+        image="@/assets/icons/yearbook.png"
       >
       </splash-card>
     </router-link>
@@ -27,7 +27,7 @@
     <router-link to="/memorabilia">
       <splash-card
         card-title="Memorabilia"
-        image="https://firebasestorage.googleapis.com/v0/b/lsfcc-project-moco.appspot.com/o/icons%2Fvarsity.png?alt=media&token=04ba7540-2f9b-4124-b5ce-25e4cf51e54e"
+        image="@/assets/icons/varsity.png"
       >
       </splash-card>
     </router-link>
@@ -35,7 +35,7 @@
     <router-link to="/transcripts">
       <splash-card
         card-title="Transcripts"
-        image="https://firebasestorage.googleapis.com/v0/b/lsfcc-project-moco.appspot.com/o/icons%2Ftranscription.png?alt=media&token=38154380-31c7-4f8a-8246-e1d043ce91e3"
+        image="@/assets/icons/transcription.png"
       >
       </splash-card>
     </router-link>
@@ -43,7 +43,7 @@
     <router-link to="/blog">
       <splash-card
         card-title="Blog"
-        image="https://firebasestorage.googleapis.com/v0/b/lsfcc-project-moco.appspot.com/o/icons%2Fblog.png?alt=media&token=9d21863f-e534-4a68-b62c-4d6edd2a3d66"
+        image="@/assets/icons/blog.png"
       >
       </splash-card>
     </router-link>
