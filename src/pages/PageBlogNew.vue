a903b7817a411e770cc10b24959d8a45be92485c
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-05-17 17:49:11 -0400

    this should be it 2

--- src/pages/PageBlogNew.vue
+++ src/pages/PageBlogNew.vue
@@ -4,7 +4,7 @@
       <v-card-title>
         New Blog Post Form
       </v-card-title>
-      <FormNewBlog :blog-key="this.$route.params.blogKey"/>
+      <FormNewBlog :blog-key="$router.params.blogKey"/>
     </template>
   </content-wrapper>
 </template>
