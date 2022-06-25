9dbd1c0b081113037e0fa082381ef730ed191785
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-06-25 11:44:56 -0400

    more cleanup for FormEdit<...> components

--- src/components/lists/ListAllProfiles.vue
+++ src/components/lists/ListAllProfiles.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
+
     <v-expansion-panels class="mb-5">
       <v-expansion-panel v-for="(profile, key) in store.profiles" :key="key">
         <ListItemProfile :profile="profile" :p-key="key" />
       </v-expansion-panel>
     </v-expansion-panels>
-  </div>
 </template>
 
 <script setup>
